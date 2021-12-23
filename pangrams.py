6bee22b2c249176d008178a3725428235ef34eca
ksaiyan <ks310796@gmail.com>
2021-12-23 09:11:01 +0530

    Create pangrams.py

---
+++ pangrams.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    res="pangram"
+    for char in alphabet:
+        if char not in s.lower():
+            res = "not pangram"
+        
+    return res
+  
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
